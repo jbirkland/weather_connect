20b295d36df81ebfea86b6a482db85e419f72059
jbirkland <jbirk05@gmail.com>
2021-10-14 23:15:12 -0500

    Readme file complete, screenshot added to folder

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # weather_connect
 # 06 Server-Side APIs: Weather Dashboard
+
+## Ojective
+To build a weather application that will run in the browser and feature dynamically updated HTML and CSS. I must use the OpenWeather One Call API to get data from and pass it to the site I am creating through javascript. Local storage will be used to store previous city's searched. Once a city name is typed and the search button is clicked it must retrieve;
+the city name,
+the date,
+an icon representation of weather conditions,
+the temperature,
+the humidity,
+the wind speed,
+the UV index,
+along with a five day forecast.
+
+### Screen Shot
+Here is the screenshot file path to my webpage. 
+![screenshot](https://user-images.githubusercontent.com/87788419/137430280-8d58aba5-4803-4e99-9ef2-f1d9cc821c5c.png)
+
+
+## Link to the Deployed Page
+
+Here is a link to my delpoyed code. https://jbirkland.github.io/weather_connect/
---
+++ assets/script.js
@@ -0,0 +1,187 @@
+var apiKey = "702dea6c4abd18dcd53720b81e5b872e";
+var citySearchBtn = $("#srchBtn");
+var { lat } = location;
+var { lon } = location;
+var ulDiv = document.getElementById("list");
+var todaysForecast = document.getElementById("currentforecast");
+var uvIndex;
+var cityStorage = JSON.parse(localStorage.getItem("cityNames")) || [];
+
+function getCityBtns(cityName) {
+  if (typeof Storage !== "undefined") {
+    let storedButtons = JSON.parse(localStorage.getItem("cityNames"));
+    if (storedButtons) {
+      for (let btnName of storedButtons) {
+        let newButton = $(
+          `<button id= '${btnName}' class='cityNames'>${btnName}</button>`
+        );
+        $("#btnsection").append(newButton);
+        // newButton.setAttribute("type", "button");
+        // newButton.setAttribute.value(("value", cityName);
+        // newButton.style.width = "200px";
+        // newButton.style.height = "50px";
+        // newButton.style.marginTop = "20px";
+        
+      }
+    }
+  }
+}
+getCityBtns();
+
+// $('#btnsection').each( function (){
+//     var buttonId = $(this)('.addCityBtn').attr('id')
+//     var task = localStorage.getItem(buttonId);
+// //     $ (this).children('.textColumn').val(task)
+// })
+// function getUvIndex(city){
+// var fiveDaysUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon=' +lon+ '&appid=702dea6c4abd18dcd53720b81e5b872e&units=imperial';
+// fetch(fiveDaysUrl)
+// .then(function(response){
+// return response.json();
+// }) .then(function(data){
+//     uvIndex = data.daily[0].uvi;
+// console.log ("")
+// })
+// }
+function getLatAndLon(cityName) {
+  var requestUrl =
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=` +
+    apiKey +
+    "&units=imperial";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+      console.log(lat);
+      console.log(lon);
+      // getUvIndex();
+      var day = moment().format("MM/DD/YYYY");
+      var header1 = document.getElementById("cityTitle");
+      var createElemUl = document.createElement("ul");
+      var createElemLi0 = document.createElement("li");
+
+      var createElemLi1 = document.createElement("li");
+      var image = document.createElement("img");
+      image.src =
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+      var createElemLi2 = document.createElement("li");
+      var createElemLi3 = document.createElement("li");
+      var createElemLi4 = document.createElement("li");
+      var createElemLi5 = document.createElement("li");
+
+      header1.textContent = data.name;
+      createElemLi0.textContent = day;
+      createElemLi1.textContent;
+      createElemLi2.textContent = "Temp: " + data.main.temp;
+      createElemLi3.textContent = "Humidity: " + data.main.humidity;
+      createElemLi4.textContent = "Wind: " + data.wind.speed;
+      createElemLi5.textContent = "UV Index: " + uvIndex;
+      console.log(uvIndex);
+
+      createElemUl.appendChild(createElemLi0);
+      createElemUl.appendChild(image);
+      createElemUl.appendChild(createElemLi2);
+      createElemUl.appendChild(createElemLi3);
+      createElemUl.appendChild(createElemLi4);
+      createElemUl.appendChild(createElemLi5);
+      ulDiv.appendChild(createElemUl);
+
+      getWeather(lat, lon);
+    });
+}
+
+function getWeather(lat, lon) {
+  var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=702dea6c4abd18dcd53720b81e5b872e`;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var slicedArr = data.daily.slice(0, 6);
+      console.log(slicedArr);
+      for (var i = 0; i < slicedArr.length; i++) {
+        var currentDay = slicedArr[i];
+      }
+      futureForecast(lat, lon);
+    });
+}
+function addCityButton(cityName) {
+  var newCityButton = document.createElement("button");
+  newCityButton.setAttribute("type", "button");
+  newCityButton.setAttribute("value", cityName);
+  newCityButton.textContent = cityName;
+  newCityButton.style.width = "200px";
+  newCityButton.style.height = "50px";
+  newCityButton.style.marginTop = "20px";
+  document.getElementById("btnsection").appendChild(newCityButton);
+  newCityButton.onclick = function () {
+    event.preventDefault();
+    getLatAndLon(cityName);
+
+    if ((ulDiv.length = 1)) {
+      ulDiv.replaceChildren();
+    } else ulDiv.append();
+  };
+}
+
+citySearchBtn.on("click", function (event) {
+  event.preventDefault();
+  var cityName = $("#cityTyped").val();
+  // getUvIndex(cityName)
+  getLatAndLon(cityName);
+
+  if ((ulDiv.length = 1)) {
+    ulDiv.replaceChildren();
+  } else {
+    ulDiv.append();
+  }
+
+  // storageArr.push(storedObject);
+  cityStorage.push(cityName);
+  localStorage.setItem("cityNames", JSON.stringify(cityStorage));
+
+  addCityButton(cityName);
+
+  //   if (typeof(Storage) !== "undefined"){
+  //       localStorage.setItem("City Buttons", newCityButton);
+  //       document.getElementById("btnsection").innerHTML =
+  //       localStorage.getItem("City Buttons");
+
+  //   } else {
+  //       document.getElementById("btnsection").innerHTML =
+  //       "This did not work";
+  //   }
+});
+
+function futureForecast(lat, lon) {
+  var fiveDaysUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=702dea6c4abd18dcd53720b81e5b872e&units=imperial";
+  fetch(fiveDaysUrl).then(function (response) {
+    return response.json().then(function (data) {
+      for (var i = 0; i < 5; i++) {
+        $("#date" + i).html(
+          moment.unix(data.daily[i].dt, "x").format("MM/DD/YYYY") +
+            "<img src='http://openweathermap.org/img/w/" +
+            data.daily[i].weather[0].icon +
+            ".png'/>"
+        );
+        $("#temp" + i).text("Temp: " + data.daily[i].temp.day + "F");
+        $("#wind" + i).text("Wind: " + data.daily[i].wind_speed + "MPH");
+        $("#humidity" + i).text("Hum:" + data.daily[i].humidity + "%");
+        uvIndex = data.daily[i].uvi;
+      }
+      uvIndex = data.daily[0].uvi;
+      console.log(uvIndex);
+    });
+  });
+}
